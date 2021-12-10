2c804fe3ebe6e3ff5936ae4d2afa2224c77f6225
SASNOKOV <67283325+SASNOKOV@users.noreply.github.com>
2021-12-10 14:24:06 +0800

    Update password.py

--- password.py
+++ password.py
@@ -1,15 +1,15 @@
 #password generator
+# made by SASNOKOV
 
 import random
-
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678910!%&*^@`~()[]{\|}12/?.,<>-+_"
-
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678910!%&*^@`~()[]{\|}12/?.,<>-+_"
 while 1:
-    password_len = int(input("what length would ur password to be : "))
-    password_count = int(input("How many passwords would you like : "))
-    for x in range(0,password_count):
+    password_length = int(input("How long is your password to be? : "))
+    number_of_passwords = int(input("How many passwords would you like? : "))
+    for x in range(0,number_of_passwords):
         password = ""
-        for x in range(0,password_len):
-            password_char = random.choice(chars)
-            password      = password + password_char
-        print("here is ur password : ", password)
+        for x in range(0,password_length):
+            password_character = random.choice(
+            characters)
+            password      = password + password_character
+        print("we present your password : ", password)
